6791dd8f52c287e1aba89dc4075550228234657e
Luis Pereira <luispereira.tkd@gmail.com>
2025-04-01 18:14:23 +0000

    theme: add styling and markup for custom theme

--- haunt.scm
+++ haunt.scm
@@ -6,7 +6,9 @@
              (haunt builder assets)
              (haunt builder redirects)
              (haunt reader commonmark)
-             (haunt publisher rsync))
+             (haunt publisher rsync)
+             (ice-9 match)
+             (srfi srfi-19))
 
 (define blog-prefix "blog")
 
@@ -15,10 +17,29 @@
     #:destination "docker/easilok-blog/dist"
     #:host "production-blog"))
 
+;; Extracts first paragraph of a post
+;; credit to dthompson (https://git.dthompson.us/blog/tree/theme.scm)
+(define (first-paragraph post)
+  (let loop ((sxml (post-sxml post)))
+    (match sxml
+      (() '())
+      (((and paragraph ('p . _)) . _)
+       (list paragraph))
+      ((head . tail)
+       (cons head (loop tail))))))
+
 ;;; Partials
 (define easilok-header
-  `(header (h1 (a (@ (href "/")) "Luís Pereira (easilok)"))
+  `(header (h1 (a (@ (href "/")) "Luis Pereira (easilok)"))
          (p "Sharing my learning journey on several technological topics.")))
+
+(define (build-tags-list post)
+  (map (lambda (tag)
+         ; TODO: add a link to a page view of posts by tags
+         ; `(a (@ (href ,(string-append "/feeds/tags/" tag ".xml"))
+         `(span (@ (class "tag-badge"))
+                "#",tag))
+       (assq-ref (post-metadata post) 'tags)))
 
 (define easilok-main-theme
   (theme #:name "easilok-main"
@@ -27,6 +48,16 @@
            `((doctype "html")
              (head
               (meta (@ (charset "utf-8")))
+              (meta (@ (name "viewport")
+                       (content "width=device-width, initial-scale=1")))
+              (link (@ (rel "icon")
+                       (type "image/png")
+                       (sizes "32x32")
+                       (href "/assets/favicon-32x32.png")))
+              (link (@ (rel "icon")
+                       (type "image/png")
+                       (sizes "16x16")
+                       (href "/assets/favicon-16x16.png")))
               (title ,(if title
                           (string-append title " :: easilok.dev")
                           (site-title site)))
@@ -34,7 +65,7 @@
               (link (@ (rel "stylesheet")
                        (href "/assets/css/normalize-801.css")))
               (link (@ (rel "stylesheet")
-                       (href "/assets/css/main.css")))
+                       (href "/assets/css/main.css"))))
              (body
                ,easilok-header
                (hr (@ (class "separator")))
@@ -43,19 +74,51 @@
               ;; Footer for the Craftering webring links
               (footer
                 (hr)
+                (p (@ (class "copyright")) "© 2025 Luís Pereira")
                 (p (em "Checkout the content from the  other "
                        (a (@ (href "https://systemcrafters.net") (target "_blank"))
                           "System Crafters") " webring members:"))
                 (div (@ (class "craftering"))
                      (a (@ (href "https://craftering.systemcrafters.net/@easilok/previous")) "←")
                      (a (@ (href "https://craftering.systemcrafters.net/")) "craftering")
-                     (a (@ (href "https://craftering.systemcrafters.net/@easilok/next")) "→")))))))))
+                     (a (@ (href "https://craftering.systemcrafters.net/@easilok/next")) "→"))))))
+         #:post-template
+         (lambda (post)
+           `((article
+               (h1 (@ (class "post__title"))
+                   ,(post-ref post 'title))
+               (div (@ (class "post__date"))
+                    ,(date->string (post-date post)
+                                   "~d ~b, ~Y"))
+               (div (@ (class "post__tags"))
+                    ,@(build-tags-list post))
+               (div (@ (class "post__content"))
+                    ,(post-sxml post)))))
+         #:collection-template
+         (lambda (site title posts prefix)
+           (define (post-uri post)
+             (string-append prefix "/" (site-post-slug site post) ".html"))
+           `((h1 ,title)
+             ,(map (lambda (post)
+                     (let ((uri (post-uri post)))
+                       `(article (@ (class "feed__post"))
+                                 (h2 (a (@ (href ,uri))
+                                        ,(post-ref post 'title)))
+                                 (div (@ (class "feed__post-date"))
+                                      ,(date->string (post-date post)
+                                                     "~d ~b, ~Y"))
+                                 (div (@ (class "feed__post-tags"))
+                                      ,@(build-tags-list post))
+                                 (div (@ (class "feed__post-summary"))
+                                      ,(first-paragraph post))
+                                 (a (@ (href ,uri)) "read post →"))))
+                   posts)))))
 
 
 (define (easilok-blog)
   (blog #:theme easilok-main-theme
         #:prefix blog-prefix
-        #:collections `(("Recent Posts" "index.html" ,posts/reverse-chronological))))
+        #:collections `(("Blog Posts" "index.html" ,posts/reverse-chronological))))
 
 (site #:title "Easilok Blog"
       #:domain "luispereira.dev"
